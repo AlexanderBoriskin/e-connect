9f0d3927b019825654350af77ca2f6eb04b89e65
Alexander Boriskin <alexander.boriskin12@gmail.com>
2018-09-20 19:27:33 +0300

    e-connect(login): create primary template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,15 +2,33 @@
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
       <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+        <div>
+          EConnect
+        </div>
+        <v-card>
+          <div style="padding: 20px 60px;">
+            <div>
+              <v-btn class="primary-btn">Log In With LinkedIn</v-btn>
+            </div>
+            <div class="text-lg-center">OR</div>
+            <div>
+              <v-text-field v-model="email" label="Email" required/>
+            </div>
+            <div>
+              <v-text-field v-model="password" label="Password" required/>
+            </div>
+            <div>
+              <v-btn class="secondary-btn">Log In With Email</v-btn>
+            </div>
+            <div class="text-lg-center">Forgot password?</div>
+          </div>
+        </v-card>
+        <div class="question-text">
+          Don't have an account?
+        </div>
+        <div>
+          <v-btn class="secondary-btn">Sign Up</v-btn>
+        </div>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
@@ -18,27 +36,40 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'HelloWorld'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+  .v-card {
+    border: 2px solid #ececec;
+  }
+
+  .v-text-field__slot {
+    border: 2px solid #ececec;
+  }
+
+  .v-input__slot:before {
+    border-style: none !important;
+  }
+
+  .question-text {
+    font-family: Gotham;
+    font-size: 24px;
+    color: #4a4a4a;
+  }
+
+  .secondary-btn {
+    background-color: #46a29f !important;
+  }
+
+  .primary-btn {
+    background-color: #147dbe !important;
+  }
+
+  .v-btn__content {
+    color: white !important;
+  }
+
 </style>
